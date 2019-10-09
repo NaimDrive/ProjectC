fcaa96fc7ea2c6d15fa685e22c32bcf25c4781e8
Gun1dex <baptisteb.benoit@hotmail.fr>
2019-10-09 18:20:18 +0200

    Avancement champions

--- type.c
+++ type.c
@@ -12,19 +12,53 @@ typedef struct champion {
   int CE;
 } Champion;
 
-Champion createChampion(char *name, int nbChampions) {
-  Champion champion;
+typedef struct arme{
+  int num;
+  char *nom;
+  int CE;
+  int CA;
+  int degats;
+  int portee;
+} Arme;
+
+typedef struct protection{
+  int num;
+  char *nom;
+  int CE;
+  int CA;
+  int probabilite;
+} Protection;
+
+typedef struct soins{
+  int num;
+  char *nom;
+  int CE;
+  int CA;
+  int volume;
+  int effet;
+} Soins;
+
+
+/*
+  Create and return a new champion.
+  @Param the champion name and the current champion number.
+  @Return the new champion. 
+*/
+Champion * createChampion(char *name, int *nbChampions) {
+  Champion *champion;
+  champion = malloc(sizeof(champion));
+
   if(strcmp(name, "Haricot") == 0) {
-    champion.num = nbChampions++;
-    champion.variete = "Haricot";
-    champion.type = "Légume";
-    champion.force = 10;
-    champion.resistance = 10;
-    champion.PVMax = 40;
-    champion.CE = 5;
-  } else if (strcmp(name, "Fenouil") == 0)
+    (*champion).num = (*nbChampions)++;
+    (*champion).variete = "Haricot";
+    (*champion).type = "Légume";
+    (*champion).force = 10;
+    (*champion).resistance = 10;
+    (*champion).PVMax = 40;
+    (*champion).CE = 5;
+  } /*else if (strcmp(name, "Fenouil") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Fenouil";
     champion.type = "Légume";
     champion.force = 12;
@@ -33,7 +67,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 7;
   } else if (strcmp(name, "Poireau") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Poireau";
     champion.type = "Légume";
     champion.force = 15;
@@ -42,7 +76,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 10;
   } else if (strcmp(name, "Aubergine") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Aubergine";
     champion.type = "Légume";
     champion.force = 20;
@@ -51,7 +85,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 15;
   } else if (strcmp(name, "Courgette") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Courgette";
     champion.type = "Légume";
     champion.force = 20;
@@ -60,7 +94,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 20;
   } else if (strcmp(name, "Carotte") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Carotte";
     champion.type = "Légume";
     champion.force = 25;
@@ -69,7 +103,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 25;
   } else if (strcmp(name, "Banane") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Banane";
     champion.type = "Fruit";
     champion.force = 10;
@@ -78,7 +112,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 5;
   } else if (strcmp(name, "Kiwi") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Kiwi";
     champion.type = "Fruit";
     champion.force = 12;
@@ -87,7 +121,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 7;
   } else if (strcmp(name, "Poire") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Poire";
     champion.type = "Fruit";
     champion.force = 15;
@@ -96,7 +130,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 10;
   } else if (strcmp(name, "Pomme") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Pomme";
     champion.type = "Fruit";
     champion.force = 20;
@@ -105,7 +139,7 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 15;
   } else if (strcmp(name, "Ananas") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Ananas";
     champion.type = "Fruit";
     champion.force = 20;
@@ -114,31 +148,48 @@ Champion createChampion(char *name, int nbChampions) {
     champion.CE = 20;
   } else if (strcmp(name, "Durian") == 0)
   {
-    champion.num = nbChampions++;
+    champion.num = (*nbChampions)++;
     champion.variete = "Durian";
     champion.type = "Fruit";
     champion.force = 25;
     champion.resistance = 25;
     champion.PVMax = 80;
     champion.CE = 25;
-  } else {
+  } */else {
     fprintf(stderr, "Champion name error\n");
     exit(1);
   }
   return champion;
 }
 
-void testAfficher(Champion c) {
-  printf("Num %d\nVariété %s\nType %s\nForce %d\nResistance %d\nPV Max %d\nCE %d\n", c.num, c.variete, c.type, c.force, c.resistance, c.PVMax, c.CE);
+void initChampions(Champion **champion, int *nbChampions, size_t sz) {
+  champion[0] = createChampion("Haricot", nbChampions);
+}
+
+void initGame(Champion **champion, int *nbChampions) {
+  initChampions(champion, nbChampions, 5);
+}
+
+void testAfficher(Champion *c) {
+  printf("Num %d\nVariété %s\nType %s\nForce %d\nResistance %d\nPV Max %d\nCE %d\n", c->num, c->variete, c->type, c->force, c->resistance, c->PVMax, c->CE);
 }
 
 int main() {
-  Champion champion;
-  int nbChampions;
+  Champion **champion;
+  int *nbChampions;
+  /*
+  Arme *arme;
+  Protection *protection;
+  Soins *soins;
+  int *nbChampions, *nbArmes, *nbProtections, *nbSoins;
+  */
+  champion = malloc(sizeof(Champion *)*5);
 
-  nbChampions = 0;
+  nbChampions = malloc(sizeof(int));
+  *nbChampions = 0;
+  
+  initGame(champion, nbChampions);
+  testAfficher(champion[0]);
 
-  champion = createChampion("Pomme",nbChampions);
-  testAfficher(champion);
   return 0;
 }

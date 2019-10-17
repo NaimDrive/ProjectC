51aad56dcb535a3a3e2e371b8a5eda2aa7459cb3
NaimDrive <naimdrive98@gmail.com>
2019-10-17 13:47:31 +0200

    commands update

--- commands.c
+++ commands.c
@@ -41,25 +41,45 @@ void showCare(int id) {
   printf("%s\n", "show care i");
 }
 
+void save() {
+  printf("%s\n", "save");
+}
+
 void quitGame() {
   exit(0);
 }
 
 void readCommands() {
-  char *command = (char*)malloc(255*sizeof(char));
+  char *command = (char*)malloc(256*sizeof(char));
+  int fakeID = 1;
+
   while(1) {
     printf("%s", "Veuillez saisir votre commande : ");
-    scanf("%s", command);
-    printf("Entered command : '%s'\n", command);
+    fgets(command, 256, stdin);
+    printf("Entered command : %s", command);
+    if((strlen(command) > 0) && (command[strlen(command)-1] == '\n')) command[strlen(command)-1] = '\0';
+
     if(strcmp(command, "quit") == 0) quitGame();
+    else if(strcmp(command, ) == 0) showVegetables();
+    else if(strcmp(command, ) == 0) showVegetables(fakeID);
+    else if(strcmp(command, ) == 0) showFruits();
+    else if(strcmp(command, ) == 0) showFruit(fakeID);
+    else if(strcmp(command, ) == 0) weapons();
+    else if(strcmp(command, ) == 0) weapon(fakeID);
+    else if(strcmp(command, ) == 0) showProtections();
+    else if(strcmp(command, ) == 0) showProtection(fakeID);
+    else if(strcmp(command, ) == 0) showCares();
+    else if(strcmp(command, ) == 0) showCare(fakeID);
+    else if(strcmp(command, "save") == 0) save();
   }
 
-  // free(command);
+  free(command);
 }
 
 int main(int argc, char *argv[]) {
 
   readCommands();
+
 
   return 0;
 

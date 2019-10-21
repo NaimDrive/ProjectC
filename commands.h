ecda440485b29ae3a2dd175af6f78affb66dae47
NaimDrive <naimdrive98@gmail.com>
2019-10-21 22:51:43 +0200

    ajout commands.h

---
+++ commands.h
@@ -0,0 +1,31 @@
+char* substring(char *src, int beg, int end);
+
+int getID(char *command, int n);
+
+void showVegetables();
+
+void showVegetable(int id);
+
+void showFruits();
+
+void showFruit(int id);
+
+void weapons();
+
+void weapon(int id);
+
+void showProtections();
+
+void showProtection(int id);
+
+void showCares();
+
+void showCare(int id);
+
+void save();
+
+void help();
+
+void quitGame();
+
+void readCommands();

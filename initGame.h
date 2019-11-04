9efa2ab7bbea237ebcdba8ccd9656e9cb3be1d82
Benoit Baptiste <baptiste_benoit@ens.univ-artois.fr>
2019-11-04 12:44:27 +0100

    Add comments

--- initGame.h
+++ initGame.h
@@ -57,87 +57,90 @@ typedef struct {
 } Team;
 
 /*
-  Create a new team
-  @return the new team
+  Create and return a new team
+  @Param1 Team
+  @Param2 int
+  @Param3 Winsize
+  @return Team
 */
 Team * initTeam(Team *team, int id, Winsize sz);
 
 /*
   Create and return a new champion.
-  @Param1 the champion name
-  @Param2 the current champion number.
-  @Return the new champion.
+  @Param1 char * (the champion name)
+  @Param2 int
+  @Return Champion
 */
 Champion * createChampion(char *name, int nbChampions);
 
 /*
   Create and return a new weapon.
-  @Param1 the weapon name
-  @Param2 the current weapon number.
-  @Return the new weapon.
+  @Param1 char * (the weapon name)
+  @Param2 int
+  @Return Weapon
 */
 Weapon * createWeapon(char *name, int nbWeapons);
 
 /*
   Create and return a new protection.
-  @Param1 the protection name
-  @Param2 the current protection number.
-  @Return the new protection.
+  @Param1 char * (the protection name)
+  @Param2 int
+  @Return Protection
 */
 Protection * createProtection(char *name, int nbProtection);
 
 /*
   Create and return a new healing.
-  @Param1 the healing name
-  @Param2 the current healing number.
-  @Return the new healing.
+  @Param1 char * (the healing name)
+  @Param2 int
+  @Return Healing
 */
 Healing * createHealing(char *name, int nbHeal);
 
 /*
-    Create and return the table of champion
-    @Param1 the table of champion
-    @Param2 the current champion number.
+    Create and return the array of champion
+    @Param1 Array of champion
+    @Param2 int
     @Return void
 */
 void initChampions(Champion **champions, int *nbChampions);
 
 /*
-    Create and return the table of weapon
-    @Param1 the table of weapon
-    @Param2 the current weapon number.
+    Create and return the array of weapon
+    @Param1 Array of weapon
+    @Param2 int
     @Return void
 */
 void initWeapons(Weapon **weapons, int *nbWeapons);
 
 /*
-    Create and return the table of protection
-    @Param1 the table of protection
-    @Param2 the current protection number.
+    Create and return the array of protection
+    @Param1 Array of protection
+    @Param2 int
     @Return void
 */
 void initProtections(Protection **protections, int *nbProtections);
 
 /*
-    Create and return the table of healing
-    @Param1 the table of healing
-    @Param2 the current healing number.
+    Create and return the array of healing
+    @Param1 Array of healing
+    @Param2 int
     @Return void
 */
 void initHealings(Healing **healings, int *nbHealings);
 
 /*
     Init game objects
-    @Param1 the table of champion
-    @Param2 the table of weapon
-    @Param3 the table of protection
-    @Param4 the table of healing
-    @Param5 the current champion number.
-    @Param6 the current weapon number.
-    @Param7 the current protection number.
-    @Param8 the current healing number.
-    @Param9 the first team.
-    @Param10 the second team.
+    @Param1 Array of champion
+    @Param2 Array of weapon
+    @Param3 Array of protection
+    @Param4 Array of healing
+    @Param5 int (champion number)
+    @Param6 int (weapon number)
+    @Param7 int (protection number)
+    @Param8 int (healing number)
+    @Param9 Team
+    @Param10 Team
     @Return Winsize
 */
 Winsize initGame(Champion **champions, Weapon **weapons, Protection **protections, Healing **healings, int *nbChampions, int *nbWeapons, int *nbProtections, int *nbHealings, Team *team1, Team *team2);

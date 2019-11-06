df2c95d7b1a82bd71d7127e68af20108fcd1ffc0
Gun1dex <baptisteb.benoit@hotmail.fr>
2019-11-07 00:22:10 +0100

    Comment added

--- displayGame.h
+++ displayGame.h
@@ -11,10 +11,31 @@ typedef struct {
     int direction;
 } AttackAnimation;
 
+/*
+    Display champions health points.
+    @Param1 Team
+    @Param2 Team
+    @Param3 int
+    @Return void
+*/
 void displayHealth(Team *team1, Team *team2, int sz);
 
+/*
+    Display champions in the map.
+    @Param1 Team
+    @Param2 Team
+    @Param3 int
+    @Return void
+*/
 void displayGame(Team *team1, Team *team2, int sz);
 
+/*
+    Display attacks in the map.
+    @Param1 Team
+    @Param2 Team
+    @Param3 int
+    @Return void
+*/
 void displayAttack(Team *team1, Team *team2, int sz);
 
 #endif
--- fightMode.h
+++ fightMode.h
@@ -17,12 +17,37 @@ void endRound(Team *team1, Team* team2, int maximumCE, int end, Winsize sz);
 
 void roundWinner(Team *team1, Team *team2, int maximumCE);
 
+/*
+    Remove the protection if it is equipped.
+    @Param Team
+    @Return void
+*/
 void takeOffProtection(Team *team);
 
+/*
+    While the duel is not finished, reset contents to fight again.
+    @Param Team
+    @Return void
+*/
 void fighNotFinished(Team *team);
 
+/*
+    The player can buy a care for the duel.
+    @Param1 Team
+    @Param2 Array with every heal
+    @Param3 int
+    @Param4 char *
+    @Return void
+*/
 void healingChoice(Team *team, Healing **healings, int *nbHealings, char *command);
 
+/*
+    Reset the team contents to play again.
+    @Param1 Team
+    @Param2 Team
+    @Param3 Winsize
+    @Return void
+*/
 void resetGame(Team *team1, Team *team2, Winsize screenSize);
 
 #endif
